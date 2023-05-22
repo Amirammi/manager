e90f040ed136a334b3509a13c5c51a78dcf34a76
Amirammi <amirammina@gmail.com>
2023-05-22 21:18:45 +0300

    Add create, update & delete view for Task

--- task_list/views.py
+++ task_list/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from .models import Task, Tag
@@ -13,3 +14,20 @@ def index(request):
 
 class TagListView(generic.ListView):
     model = Tag
+
+
+class TaskCreate(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("task_list:index")
+
+
+class TaskUpdate(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("task_list:index")
+
+
+class TaskDelete(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("task_list:index")
