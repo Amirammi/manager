928b5a94a319505421a341d08f3b89d3338c5677
Amirammi <amirammina@gmail.com>
2023-05-22 18:54:54 +0300

    Add path to tag-list in urls

--- task_list/urls.py
+++ task_list/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.urls import path
 
-from .views import index
+from .views import index, TagListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
 ]
 
 app_name = "task_list"
