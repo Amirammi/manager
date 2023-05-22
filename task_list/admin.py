b84b7eae249af5eeeb6360449fbf12c708d5e298
Amirammi <amirammina@gmail.com>
2023-05-22 16:42:53 +0300

    Registered Task % Tag models in admin.py

--- task_list/admin.py
+++ task_list/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from models import Task, Tag
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "datetime",
+        "is_completed",
+        "deadline",
+    )
+    list_filter = ("is_completed",)
+    search_fields = ("name",)
+
+
+admin.site.register(Tag)
+admin.site.unregister(Group)
