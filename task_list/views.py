77b05057a1838154159c2c682816192957fc0269
Amirammi <amirammina@gmail.com>
2023-05-22 16:57:44 +0300

    Add index func for home page & TagListView

--- task_list/views.py
+++ task_list/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Task, Tag
+
+
+def index(request):
+    context = {
+        "tasks": Task.objects.prefetch_related("tags"),
+    }
+    return render(request, "task_list/index.html", context=context)
+
+
+class TagListView(generic.ListView):
+    model = Tag
