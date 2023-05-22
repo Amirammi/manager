291b311bd835d5c7ac140aedfc0f2248f6faeb11
Amirammi <amirammina@gmail.com>
2023-05-22 16:37:58 +0300

    Add Tag & Task model to models.py

--- task_list/models.py
+++ task_list/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=63)
+
+
+class Task(models.Model):
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateField()
+    is_completed = models.BooleanField()
+    tag = models.ManyToManyField(
+        Tag,
+        related_name="tasks"
+    )
